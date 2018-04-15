bbd224ffd516fa58da89389a7decf419c422e897
xieliang <1186654762@qq.com>
2018-04-16 00:06:30 +0800

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,171 +1,287 @@
+c
 <template>
-    <div class="login-container">
-        <div class="main">
-            <div class="login-img">
-                <img src="../assets/login.png">
-                <h2>成就所托，持之以恒!</h2>
-                <h3>洞悉需求，应势而生；风雨同舟，一路同行</h3>
-            </div>
-            <div class="login-box">
-                <h3>业务基础框架</h3>
-                <div class="login-content">
-                    <h3>用户登录</h3>
-                    <el-form>
-                        <el-form-item>
-                            <el-input placeholder="邮箱/用户名"></el-input>
-                        </el-form-item>
-                        <el-form-item>
-                            <el-input placeholder="密码"></el-input>
-                        </el-form-item>
-                        <div class="check-box">
-                            <el-form-item>
-                                <el-input placeholder="验证码"></el-input>
-                            </el-form-item>
-                            <div class="checkcode">验证码</div>
-                        </div>
-                        <el-button type="primary">登录</el-button>
-                    </el-form>
+  <div class="login-container">
+    <div class="main">
+      <div class="login-img">
+        <img src="../assets/login.png">
+        <h2>成就所托，持之以恒!</h2>
+        <h3>洞悉需求，应势而生；风雨同舟，一路同行</h3>
+      </div>
+      <div class="login-box">
+        <h3>业务基础框架</h3>
+        <div class="login-content">
+          <h3>用户登录</h3>
+          <el-form>
+            <el-form-item>
+              <el-input placeholder="邮箱/用户名"
+                        v-model="loginForm.username"
+                        @blur="nameBlur"></el-input>
+              <div class="verify-tip verify-bottom" v-show='verifyTip.userError'>
+                <div class="verify-tip-arrow"></div>
+                <div class="verify-tip-inner">{{verifyTip.userErrorInfo}}</div>
+              </div>
+            </el-form-item>
+            <el-form-item>
+              <el-input placeholder="密码"
+                        type="password"
+                        v-model="loginForm.password"></el-input>
+              <div class="verify-tip verify-bottom" v-show='verifyTip.passError'>
+                <div class="verify-tip-arrow"></div>
+                <div class="verify-tip-inner">{{verifyTip.passErrorInfo}}</div>
+              </div>
+            </el-form-item>
+            <div class="check-box">
+              <el-form-item>
+                <el-input placeholder="验证码"
+                          v-model="loginForm.checkCode"></el-input>
+                <div class="verify-tip verify-bottom" v-show='verifyTip.codeError'>
+                  <div class="verify-tip-arrow"></div>
+                  <div class="verify-tip-inner">{{verifyTip.codeErrorInfo}}</div>
                 </div>
+              </el-form-item>
+              <div class="checkCode">
+                <canvas id="codeImg"
+                        title="点击这里刷新验证码"
+                        @click="refreshCode"></canvas>
+              </div>
             </div>
+            <el-button type="primary">登录</el-button>
+          </el-form>
         </div>
-        <div class="footer">@2018 vue练习项目</div>
+      </div>
     </div>
+    <div class="footer">@2018 vue练习项目</div>
+  </div>
 </template>
 
 <script>
   import httpFetch from '../api/httpFetch'
+  import Cookie from 'js-cookie'
 
   export default {
     name: 'Login',
     data() {
-      return {}
+      return {
+        randomCode: '',
+        loginForm: {
+          username: 'admin',
+          password: '111111',
+          checkCode: ''
+        },
+        verifyTip: {
+          userError: false,
+          userErrorInfo: '',
+          passError: false,
+          passErrorInfo: '',
+          codeError: false,
+          codeErrorInfo: ''
+
+        }
+      }
     },
     created() {
+      document.body.style.background = '#16191E'
+    },
+    mounted() {
       document.body.style.background = '#16191E'
       this.createCode()
     },
     destroyed() {
       document.body.style.background = '#FFFFFF'
     },
+    watch: {
+      loginForm: {
+        handler() {
+          this.verifyTip.userError = false
+          this.verifyTip.userErrorInfo = ''
+          this.verifyTip.passError = false
+          this.verifyTip.passErrorInfo = ''
+          this.verifyTip.codeError = false
+          this.verifyTip.codeErrorInfo = ''
+        },
+        deep: true
+      }
+    },
     methods: {
+      refreshCode() {
+        this.createCode()
+      },
       createCode() {
         httpFetch.get('/getCheckCode.json').then(res => {
           if (res && res.data) {
             const data = res.data
+            const canvas = document.getElementById('codeImg')
+            const context = canvas.getContext('2d')
+            // 每次生成code先将其清空防止叠加
+            context.clearRect(0, 0, canvas.width, canvas.height)
+            context.strokeStyle = '#FFF'
+            context.strokeRect(0, 0, canvas.width, canvas.height)
             const ranCode = data.code
-            alert(ranCode)
+            if (ranCode === '') return
+            this.randomCode = ranCode
+            this.loginForm.checkCode = ranCode
+            Cookie.set('checkCode-token', data.sessionId)
+            // 生成干扰线，数量随意
+            for (var i = 0; i < 20; i++) {
+              this.drawline(canvas, context)
+            }
+            // 循环生成4位验证码
+            for (let k = 0; k < ranCode.length; k++) {
+              context.font = '76px Arial'
+              // 将初始状态保存
+              context.save()
+              // 获得-1到1的随机数
+              var rA = 1 - Math.random() * 2
+              // 获取随机倾斜角
+              var angle = rA / 8
+              var ranNum = ranCode.charAt(k)
+              // 旋转生成的随机字符
+              context.rotate(angle)
+              // 把rand()生成的随机数文本依次填充到canvas中，注意x坐标
+              context.fillText(ranNum, 60 + 45 * k, 100)
+              // 恢复初始状态，以便下一次循环
+              context.restore()
+            }
           }
         })
+      },
+      /* 随机干扰线条函数*/
+      drawline(canvas, context) {
+        // 若省略beginPath，则每点击一次验证码会累积干扰线的条数
+        context.beginPath()
+        // 起点与终点在canvas宽高内随机
+        context.moveTo(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height))
+        context.lineTo(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height))
+        context.lineWidth = 1
+        context.strokeStyle = '#275DB3'
+        context.stroke()
+      },
+      nameBlur() {
+        if (this.loginForm.username === /** @type {boolean} */ '') {
+          this.verifyTip.userErrorInfo = '用户名不能为空'
+          this.verifyTip.userError = true
+        }
       }
     }
   }
 </script>
 <style scoped lang="scss">
-    @import "../styles/variables";
+  @import "../styles/variables";
+  @import "../styles/mixin";
 
-    .login-container {
-        position: relative;
-        width: 85%;
-        min-height: 100%;
-        margin: 0 auto;
-        .main {
-            display: flex;
-            padding-top: 132px;
-            height: 100%;
-            .login-img {
-                flex: 1;
-                height: 100%;
-                h2 {
-                    font-size: 28px;
-                    color: #5C9CF5;
-                    text-align: center;
-                    margin-top: -40px;
-                }
-                h3 {
-                    font-size: 16px;
-                    color: #4569A2;
-                    text-align: center;
-                    margin-top: 15px;
-                }
-            }
-            .login-box {
-                flex: 0 0 380px;
-                margin-left: 20px;
-                h3 {
-                    width: 100%;
-                    text-align: center;
-                    color: #ffffff;
-                    font-size: 26px;
-                    margin-bottom: 20px;
-                }
-                .login-content {
-                    width: 380px;
-                    height: 360px;
-                    background: #ffffff;
-                    border-radius: 8px;
-                    h3 {
-                        width: 360px;
-                        height: 50px;
-                        line-height: 50px;
-                        color: #4a94fa;
-                        text-align: center;
-                        font-size: $font-size-large;
-                        font-weight: normal;
-                        border-bottom: 1px solid $color-text-ll;
-                        margin-left: 10px;
-                        margin-right: 10px;
-                    }
-                    .el-form {
-                        margin: 20px 40px;
-                        .check-box {
-                            display: flex;
-                            .el-form-item {
-                                display: inline-block;
-                                flex: 1;
-                            }
-                            .checkcode {
-                                display: inline-block;
-                                width: 120px;
-                                background: yellowgreen;
-                                height: 45px;
-                                flex: 0 0 120px;
-                                margin-left: 10px;
-                                text-align: center;
-                                line-height: 45px;
-                                color: $color-text;
-                            }
-                        }
-                        .el-button {
-                            display: block;
-                            width: 100%;
-                            height: 42px;
-                            margin-top: 10px;
-                        }
-                    }
-                }
-            }
+  .verify-tip {
+    @include verify-tip()
+  }
+
+  .login-container {
+    position: relative;
+    width: 85%;
+    min-height: 100%;
+    margin: 0 auto;
+    .main {
+      display: flex;
+      padding-top: 132px;
+      height: 100%;
+      .login-img {
+        flex: 1;
+        height: 100%;
+        text-align: center;
+        h2 {
+          font-size: 28px;
+          color: #5C9CF5;
+          text-align: center;
+          margin-top: -40px;
         }
-        .footer {
-            position: absolute;
+        h3 {
+          font-size: 16px;
+          color: #4569A2;
+          text-align: center;
+          margin-top: 15px;
+        }
+      }
+      .login-box {
+        flex: 0 0 380px;
+        margin-left: 20px;
+        h3 {
+          width: 100%;
+          text-align: center;
+          color: #ffffff;
+          font-size: 26px;
+          margin-bottom: 20px;
+        }
+        .login-content {
+          width: 380px;
+          height: 360px;
+          background: #ffffff;
+          border-radius: 8px;
+          h3 {
+            width: 360px;
             height: 50px;
             line-height: 50px;
-            bottom: 0;
+            color: #4a94fa;
             text-align: center;
-            color: $color-text-l;
-            font-size: $font-size-medium;
-            left: 50%;
-            transform: translateX(-50%);
+            font-size: $font-size-large;
+            font-weight: normal;
+            border-bottom: 1px solid $color-text-ll;
+            margin-left: 10px;
+            margin-right: 10px;
+          }
+          .el-form {
+            margin: 20px 40px;
+            .check-box {
+              display: flex;
+              .el-form-item {
+                display: inline-block;
+                flex: 1;
+              }
+              .checkCode {
+                display: inline-block;
+                width: 120px;
+                height: 45px;
+                flex: 0 0 120px;
+                margin-left: 10px;
+                text-align: center;
+                line-height: 45px;
+                color: $color-text;
+                padding: 5px;
+                #codeImg {
+                  width: 100%;
+                  height: 100%;
+                  cursor: pointer;
+                }
+              }
+            }
+            .el-button {
+              display: block;
+              width: 100%;
+              height: 42px;
+              margin-top: 10px;
+            }
+          }
         }
+      }
     }
+    .footer {
+      position: absolute;
+      height: 50px;
+      line-height: 50px;
+      bottom: 0;
+      text-align: center;
+      color: $color-text-l;
+      font-size: $font-size-medium;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
 </style>
 <style lang="scss">
-    /* reset element-ui css */
-    .login-container {
-        .el-input {
-            input {
-                display: inline-block;
-                height: 45px;
-            }
-        }
+  /* reset element-ui css */
+  .login-container {
+    .el-input {
+      input {
+        display: inline-block;
+        height: 45px;
+      }
     }
+  }
 </style>
